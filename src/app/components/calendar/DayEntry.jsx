1e0f2e472fdef6dd5f53d5b5d76878c4146a60cd
Marko Bilal <marko.barca10@gmail.com>
2018-03-20 12:49:28 -0400

    favicon

--- src/app/components/calendar/DayEntry.jsx
+++ src/app/components/calendar/DayEntry.jsx
@@ -12,38 +12,35 @@ const getStyle = (list, idx) => ({
 export default class DayEntry extends React.Component {
   state={ cardActive: false, currentWorkout: {} }
 
-  toggleCard = (active, workout) => ({ target }) => {
-    if (!active) {
+  closeCard = () => this.setState(state => ({
+    ...state,
+    cardActive: !this.state.cardActive,
+  }));
+  openCard = workout => ({ target }) => {
+    this.setState(state => ({
+      ...state,
+      currentWorkout: workout,
+    }), () => {
+      const pos = target.getBoundingClientRect();
+      const cardPosition = {
+        top: pos.top + pos.height - 24,
+        left: pos.left + pos.width + 16,
+      };
+
+      const horizDiff = document.documentElement.clientWidth - (pos.right + 245 + 24);
+      const vertDiff = (document.documentElement.clientHeight - pos.top - 16) - 300;
+      if (vertDiff < 0) {
+        cardPosition.top += vertDiff - 2;
+      }
+      if (horizDiff < 0) {
+        cardPosition.left = pos.left - 245 - 16;
+      }
       this.setState(state => ({
         ...state,
+        cardPosition,
         cardActive: !this.state.cardActive,
       }));
-    } else {
-      this.setState(state => ({
-        ...state,
-        currentWorkout: workout,
-      }), () => {
-        const pos = target.getBoundingClientRect();
-        const cardPosition = {
-          top: pos.top + pos.height - 24,
-          left: pos.left + pos.width + 16,
-        };
-
-        const horizDiff = document.documentElement.clientWidth - (pos.right + 245 + 24);
-        const vertDiff = (document.documentElement.clientHeight - pos.top - 16) - 300;
-        if (vertDiff < 0) {
-          cardPosition.top += vertDiff - 2;
-        }
-        if (horizDiff < 0) {
-          cardPosition.left = pos.left - 245 - 16;
-        }
-        this.setState(state => ({
-          ...state,
-          cardPosition,
-          cardActive: !this.state.cardActive,
-        }));
-      });
-    }
+    });
   }
 
   render() {
@@ -62,8 +59,8 @@ export default class DayEntry extends React.Component {
           <Box
             id={`hover${i}`}
             key={`${day.format('D') + i}`}
-            onMouseEnter={this.toggleCard(true, w)}
-            onMouseLeave={this.toggleCard(false)}
+            onMouseEnter={this.openCard(w)}
+            onMouseLeave={this.closeCard}
           >
             <Box className={s.strip} style={getStyle(s.stripColors.split(','), w.styleIdx)} />
             <Box
